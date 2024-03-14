9d15f69f62bca7d0c681c1a743c6cd9bceafcc87
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-03-15 01:06:17 +0600

    conditional css applied

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -11,8 +11,8 @@ const handleVisited = ()=>{
 }
 
     return (
-        <div className='country'>
-<h3>Name: {name?.common}</h3>
+        <div className={`country ${visited ? 'visited':"non-visited"}`}>
+<h3 style={{color: visited ? 'red':'black'}} >Name: {name?.common}</h3>
 <img src={flags.png} alt="" />
 <p>Population:{population} </p>
 <p>Area : {area}</p>
